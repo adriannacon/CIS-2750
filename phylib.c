609b4e293f27b93e42774d5a4d6b0a6433ed3367
acon <acon@uoguelph.ca>
2024-01-22 11:07:08 -0500

    wrote phylib_copy_object

--- phylib.c
+++ phylib.c
@@ -1,5 +1,24 @@
 #include "phylib.h"
 
 #include <stdio.h>
+#include <string.h>
 #include <math.h>
+
+void phylib_copy_object(phylib_object **dest, phylib_object **src){
+    if (*src == NULL){ //check if src points to NULL
+        *dest = NULL; //sets dest to NULL
+    } else{
+        //allocate mem for the new obj
+        *dest = (phylib_object *)malloc(sizeof(phylib_object));
+
+        //check if malloc worked
+        if (*dest == NULL){
+            *dest = NULL; //dest set to NULL
+        } else{
+            //memcpy the contents of obj
+            //Reference: https://cplusplus.com/reference/cstring/memcpy/
+            memcpy(*dest, *src, sizeof(phylib_object));
+        }
+    }
+}
 
