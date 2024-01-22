2b509103f56f257d16b87951efbe459a33256cee
acon <acon@uoguelph.ca>
2024-01-22 12:51:56 -0500

    wrote phylib_add_object

--- phylib.c
+++ phylib.c
@@ -1,10 +1,11 @@
 #include "phylib.h"
 
+#include <stdin.h>
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
 
-void phylib_copy_object(phylib_object **dest, phylib_object **src){
+void phylib_copy_object( phylib_object **dest, phylib_object **src){
     if (*src == NULL){ //check if src points to NULL
         *dest = NULL; //sets dest to NULL
     } else{
@@ -37,4 +38,26 @@ phylib_table *phylib_copy_table( phylib_table *table){
     }
 
     return copiedTable; //return copied table
+}
+
+void phylib_add_object( phylib_table *table, phylib_object *object){
+    if (table == NULL || object == NULL){ //check if inputted table or obj is NULL
+        return; //do nothing
+    }
+
+    //iterate over the obj array until NULL 
+    for (int i =0; i < PHYLIB_MAX_OBJECTS; i++){
+        if (table->object[i] == NULL){
+            //assign to the address of the obj
+            table->object[i] = object;
+            //object added
+            return; //exit
+        }
+    }
+
+    //if no null pointer-> do nothing
+}
+
+void phylib_free_table( phylib_table *table){
+
 }
